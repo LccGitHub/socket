c0628b7aa2a3d876d86a49b1efab7afa7559cc7c
lichengcheng <lichengcheng@pset.suntec.net>
2018-03-21 18:33:04 +0800

    demo for sock epoll
    Change-Id: I8e78c196d7adfc45904a2e0ca4cbc1f21a18eabc

---
+++ epoll/client.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+
+#define PORT 1024
+#define BUFFER_1K 1024
+
+int main(int argc, char** argv)
+{
+	int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_fd < 0) {
+		perror("socket failed:");
+		exit(1);
+	}
+
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	if (inet_aton("127.0.0.1", &server_addr.sin_addr) ==0) {
+		printf("server ip error \n");
+		exit(1);
+	}
+
+	if (connect(client_fd, (struct sockaddr*)&server_addr, (socklen_t)sizeof(server_addr)) < 0) {
+		perror("connect failed:");
+		close(client_fd);
+		exit(1);
+	}
+
+	printf("connect success ....");
+
+	while(1) {
+		char buffer[BUFFER_1K];
+		memset(buffer, 0, sizeof(buffer));
+		write(client_fd, "12345678", 20);
+		int length = read(client_fd, buffer, sizeof(buffer));
+		if (length < 0) {
+			perror("read failed:");
+			close(client_fd);
+			exit(1);
+		}
+		printf("read buf:%s\n", buffer);
+	}
+	return 0;
+}
+
