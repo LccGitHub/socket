266fd813a4771fd480ca8e48305f36fa3fbc1ada
lichengcheng <lichengcheng@pset.suntec.net>
2018-07-06 14:36:48 +0800

    add server socket to class Server
    Change-Id: If46a48f35ec9a95fae1ea4cbf98f76805c959514

--- poll/server_socket.cpp
+++ poll/server_socket.cpp
@@ -7,6 +7,7 @@
 #include<string.h>
 #include<vector>
 #include<unistd.h>
+#include<pthread.h>
 #define POLL_SIZE 32
 
 class ClientInfo
@@ -55,19 +56,21 @@ void addClientInfo(ClientInfo* clientInfo)
 		mClientInfo.push_back(clientInfo);
 		printf("this client[%p] have add \n", clientInfo);
 	}
-	printf("add Client, current mClientInfo size is %d \n", mClientInfo.size());
+	printf("add Client, current mClientInfo size is %zd \n", mClientInfo.size());
 }
 void removeClientInfo(ClientInfo* clientInfo)
 {
-	for(auto iter = mClientInfo.begin(); iter <= mClientInfo.end();) {
+	printf("remove Client, befor mClientInfo size is %zd \n", mClientInfo.size());
+	for(auto iter = mClientInfo.begin(); iter < mClientInfo.end();) {
 		if (*iter == clientInfo) {
 			iter = mClientInfo.erase(iter);
+			printf("this client have erase \n");
 		}
 		else {
 			iter++;
 		}
 	}
-	printf("remove Client, current mClientInfo size is %d \n", mClientInfo.size());
+	printf("remove Client, after mClientInfo size is %zd \n", mClientInfo.size());
 }
 ClientInfo* findClientByFd(int fd)
 {
@@ -102,8 +105,127 @@ void matchDataByFd(int fd, char* data)
 	ClientInfo* client = findClientByFd(fd);
 	client->matchReadData(data);
 }
+
+class Server
+{
+	public:
+		Server()
+		{
+			createSocket();
+			printf("create Server \n");
+		}
+		~Server()
+		{
+			closeServer();
+			printf("destory Server \n");
+		}
+		int createSocket()
+		{
+			int result = 0;
+			struct sockaddr_in server_addr;
+			memset(&server_addr, 0, sizeof(server_addr));
+			server_addr.sin_family = AF_INET; //set ip protocol
+			server_addr.sin_addr.s_addr = INADDR_ANY; //server ip addr--allow connect any ip addr
+			server_addr.sin_port = htons(8000);// server port
+			int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+			if (server_fd < 0) {
+				perror("create socket failed:");
+				result = -1;
+			}
+			else if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr)) < 0){
+				perror("bind error:");
+				result = -1;
+			}
+			int opt = 1;
+			setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int));
+			mServerFd = server_fd;
+			return result;
+
+		}
+		void startWork()
+		{
+			struct sockaddr_in client_addr;
+			listen(mServerFd, 20);
+			int numfds = 0;
+			int maxfd = 0;
+			//struct pollfd mPollSet[POLL_SIZE];
+			mPollSet[0].fd = mServerFd;
+			mPollSet[0].events = POLLIN;
+			numfds++;
+			while(1){
+				printf("wait for client(total [%d])...\n", numfds);
+				poll(mPollSet, numfds, -1);
+				int index = 0;
+				for (index = 0; index < numfds; index++) {
+					if (mPollSet[index].revents & POLLIN) {
+						if (mPollSet[index].fd == mServerFd) {
+							socklen_t client_len =  sizeof(client_addr);
+							int client_fd = accept(mServerFd, (struct sockaddr*)&client_addr, &client_len);
+
+							mPollSet[numfds].fd = client_fd;
+							mPollSet[numfds].events = POLLIN;
+							numfds++;
+							ClientInfo* client = new ClientInfo(client_fd);
+							addClientInfo(client);
+							printf("add Client on fd %d\n", client_fd);
+						}
+						else {
+							char buffer[1024] = {0};
+							memset(buffer,0, sizeof(buffer));
+							int res = -1;
+							res = read(mPollSet[index].fd, buffer, sizeof(buffer)-1);
+							printf("have receive cleint[%d], data[%s], res[%d]\n", mPollSet[index].fd, buffer, res);
+							matchDataByFd(mPollSet[index].fd, buffer);
+							if (res > 0) {
+								write(mPollSet[index].fd, "received!", strlen("received!"));
+							}
+							else if (res == 0) {
+								printf("this client[%d] have close\n", mPollSet[index].fd);
+								close(mPollSet[index].fd);
+								removeClientInfoByFd(mPollSet[index].fd);
+#if 0
+								mPollSet[index].events = 0;
+								mPollSet[index].fd = -1;
+#endif
+								int i = 0;
+#if 1
+								for(i = index; i< numfds-1; i++) {
+									//mPollSet[index] = mPollSet[index+1];
+									memcpy(&mPollSet[index], &mPollSet[index+1], sizeof(mPollSet));
+								}
+								numfds--;
+#endif
+							}
+							else {
+								perror("read error:");
+							}
+						}
+					}
+
+				}
+			}
+		}
+		void closetServer()
+		{
+			close(mServerFd);
+		}
+	private:
+		int mServerFd;
+		struct pollfd mPollSet[POLL_SIZE];
+};
+
+void* serverHandler(void* args)
+{
+	Server* server = new Server();
+	server->startWork();
+	return NULL;
+}
 int main()
 {
+	pthread_t server_thr;
+	pthread_create(&server_thr, NULL, serverHandler, NULL);
+	pthread_join(server_thr, NULL);
+#if 0
 	struct sockaddr_in server_addr;
 	struct sockaddr_in client_addr;
 	memset(&server_addr, 0, sizeof(server_addr));
@@ -178,6 +300,7 @@ int main()
 
 		}
 	}
+#endif
 
 	return 0;
 }
